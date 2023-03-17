ff0db6ebec13466a7e6d4f126cb346d780a93bc3
Tayfun Adakoğlu <tayfun.adakoglu@hahn-softwareentwicklung.de>
2023-03-17 22:48:49 +0300

    fixed issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -6,10 +5,6 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'login',
     component: LoginComponent
   },
 ];
